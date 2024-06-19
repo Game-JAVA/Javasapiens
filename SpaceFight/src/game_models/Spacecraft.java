d60a1698f6ad1a3abd4ab1ee2ae382b637e315d9
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-06-19 14:22:41 -0300

    Revert "fix: code recovery"
    This reverts commit f88076c11e3f9cbcdd13e55ed5084c86bb423336.

--- SpaceFight/src/game_models/Spacecraft.java
+++ SpaceFight/src/game_models/Spacecraft.java
@@ -5,8 +5,6 @@ import java.awt.event.KeyEvent;
 public class Spacecraft extends Character {
 
     private int dx, dy;
-    private final int SCREEN_WIDTH = 1024;  // Largura da tela
-    private final int SCREEN_HEIGHT = 728;  // Altura da tela
 
     //Pegando x e y do pai
     public Spacecraft(int x, int y) {
@@ -20,17 +18,9 @@ public class Spacecraft extends Character {
     }
 
     //Movimentação da nave
-    public void move() {
-        int newX = getX() + dx;
-        int newY = getY() + dy;
-
-        // Verificar se a nova posição está dentro dos limites da tela
-        if (newX >= 0 - (getWidth()*0.38) && newX <= SCREEN_WIDTH - (getWidth()*0.6)) {
-            super.setX(newX);
-        }
-        if (newY >= 0 - (getHeight()*0.5) && newY <= SCREEN_HEIGHT - (getHeight()*0.75)) {
-            super.setY(newY);
-        }
+    public void move(){
+        super.setX(getX() + dx);
+        super.setY(getY() + dy);
     }
 
     //Reconhecer quando a tecla está pressionada
